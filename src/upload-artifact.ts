8c6e7a17b95916b6d62fe31c43a86d5aa434d28a
Nguyễn Văn Hải <hainv053@gmail.com>
2025-02-13 17:34:01 +0700

    Init

---
+++ src/upload-artifact.ts
@@ -0,0 +1,128 @@
+import * as core from "@actions/core";
+import * as fs from "fs";
+import * as mime from "mime-types";
+import * as path from "path";
+
+import { findFilesToUpload } from "./search";
+import { getInputs } from "./input-helper";
+import { NoFileOptions } from "./constants";
+
+import { S3Client } from "@aws-sdk/client-s3";
+import { Upload } from "@aws-sdk/lib-storage";
+
+async function run(): Promise<void> {
+  try {
+    const inputs = getInputs();
+    const searchResult = await findFilesToUpload(inputs.searchPath);
+
+    if (searchResult.filesToUpload.length === 0) {
+      // No files were found. Handle according to the input option.
+      switch (inputs.ifNoFilesFound) {
+        case NoFileOptions.warn: {
+          core.warning(
+            `No files were found with the provided path: ${inputs.searchPath}. No artifacts will be uploaded.`,
+          );
+          break;
+        }
+        case NoFileOptions.error: {
+          core.setFailed(
+            `No files were found with the provided path: ${inputs.searchPath}. No artifacts will be uploaded.`,
+          );
+          break;
+        }
+        case NoFileOptions.ignore: {
+          core.info(
+            `No files were found with the provided path: ${inputs.searchPath}. No artifacts will be uploaded.`,
+          );
+          break;
+        }
+      }
+    } else {
+      // Create the S3 client using AWS SDK v3.
+      const s3Client = new S3Client({
+        region: inputs.region,
+        endpoint: inputs.s3Endpoint,
+        maxAttempts: 10,
+      });
+
+      if (inputs.s3Prefix !== "") {
+        core.info("NOTE: s3-prefix specified, ignoring name parameter");
+      }
+
+      // If s3Prefix is left blank, use a default value based on the GitHub context.
+      const s3Prefix = inputs.s3Prefix;
+
+      const fileCount = searchResult.filesToUpload.length;
+      const pluralSuffix = fileCount === 1 ? "" : "s";
+      core.info(
+        `With the provided path, there will be ${fileCount} file${pluralSuffix} uploaded`,
+      );
+      core.info(`Uploading to S3 prefix: ${s3Prefix}`);
+      core.debug(`Root artifact directory is ${searchResult.rootDirectory}`);
+
+      const retentionDays = inputs.retentionDays || 90;
+      const today = new Date();
+      const expirationDate = new Date(today);
+      expirationDate.setDate(expirationDate.getDate() + retentionDays);
+
+      // Iterate over all files to be uploaded.
+      for await (const fileName of searchResult.filesToUpload) {
+        core.debug(
+          JSON.stringify({
+            rootDirectory: searchResult.rootDirectory,
+            fileName,
+          }),
+        );
+
+        // Derive the relative file path.
+        const relativeName = fileName.replace(
+          String.raw`${searchResult.rootDirectory}${path.sep}`,
+          "",
+        );
+        // Construct the S3 key (using forward slashes).
+        const uploadKey = `${s3Prefix}/${relativeName}`.replace(
+          new RegExp(`\\${path.sep}`, "g"),
+          "/",
+        );
+
+        // Create a read stream for the file.
+        const fileStream = fs.createReadStream(fileName);
+        const contentType = mime.lookup(uploadKey).toString();
+
+        // Set the upload parameters.
+        const uploadParams = {
+          Bucket: inputs.s3Bucket,
+          Key: uploadKey,
+          Body: fileStream,
+          Expires: expirationDate,
+          ContentType: contentType,
+        };
+
+        // Configure multipart upload options.
+        const uploadOptions = {
+          client: s3Client,
+          params: uploadParams,
+          queueSize: 5, // Maximum concurrent parts
+          partSize: 10 * 1024 * 1024, // 10 MB per part
+        };
+
+        core.info(`Starting upload of ${relativeName}`);
+
+        try {
+          // Create a new Upload instance and wait for it to complete.
+          const parallelUploader = new Upload(uploadOptions);
+          await parallelUploader.done();
+        } catch (err: any) {
+          core.error(`Error uploading ${relativeName}: ${err.message}`);
+          throw err;
+        } finally {
+          core.info(`Finished upload of ${relativeName}`);
+        }
+      }
+    }
+  } catch (err: any) {
+    core.setFailed(err.message);
+  }
+}
+
+run();
---
+++ src/upload-inputs.ts
@@ -0,0 +1,43 @@
+import { NoFileOptions } from "./constants";
+
+export interface UploadInputs {
+  /**
+   * The name of the artifact that will be uploaded
+   */
+  artifactName: string;
+
+  /**
+   * The search path used to describe what to upload as part of the artifact
+   */
+  searchPath: string;
+
+  /**
+   * The desired behavior if no files are found with the provided search path
+   */
+  ifNoFilesFound: NoFileOptions;
+
+  /**
+   * Duration after which artifact will expire in days
+   */
+  retentionDays: number;
+
+  /**
+   * S3 Bucket to uploads to
+   */
+  s3Bucket: string;
+
+  /**
+   * S3 Prefix to upload to
+   */
+  s3Prefix: string;
+
+  /**
+   * S3 Endpoint URL. This is useful for S3 compatible services like Minio
+   */
+  s3Endpoint: string;
+
+  /**
+   * AWS region where your s3 bucket lives
+   */
+  region: string;
+}
